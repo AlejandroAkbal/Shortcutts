d26050611cb79e73c392af395af97dd60e76cc6d
Alejandro Akbal <37181533+VoidlessSeven7@users.noreply.github.com>
2019-12-22 13:57:46 +0100

    Configured settings, Added default packages for my PWAs

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Shortcutts',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: 'Learning shortcuts made easy'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
@@ -19,7 +19,7 @@ export default {
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: '#181a1b' },
   /*
    ** Global CSS
    */
@@ -40,7 +40,44 @@ export default {
   /*
    ** Nuxt.js modules
    */
-  modules: ['@nuxtjs/pwa'],
+  modules: ['@nuxtjs/pwa', 'nuxt-purgecss', '@nuxtjs/sitemap'],
+  /*
+   ** Progressive web app
+   */
+  pwa: {
+    manifest: {
+      name: 'Shortcutts',
+      short_name: 'Shortcutts',
+      lang: 'en',
+      theme_color: '#181a1b',
+      background_color: '#181a1b'
+    },
+    meta: {
+      /* meta options */
+      ogHost: 'https://shortcutts.app',
+      mobileAppIOS: true
+    }
+  },
+  /*
+   ** PurgeCSS config
+   */
+  purgeCSS: {
+    // whitelist: ['defaults-and-this-class']
+    whitelistPatterns: [/active/, /fade/, /enter/, /leave/]
+  },
+  /*
+   ** Sitemap configuration
+   */
+  sitemap: {
+    hostname: 'https://shortcutts.app',
+    gzip: true,
+    defaults: {
+      changefreq: 'daily',
+      priority: 1,
+      lastmod: new Date(),
+      lastmodrealtime: true
+    }
+  },
   /*
    ** Build configuration
    */
@@ -48,6 +85,17 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    // Necessary for CSS Purge
+    extractCSS: true,
+
+    //  Remove console.log everywhere
+    terser: {
+      terserOptions: {
+        compress: {
+          drop_console: true
+        }
+      }
+    }
+    // extend(config, ctx) {}
   }
 }
